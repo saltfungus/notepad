52a10625295982ea14b620188fb622e3e52bb053
saltfungus <77501069+saltfungus@users.noreply.github.com>
2021-01-24 17:52:20 +1100

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,71 @@
+clear
+echo 'starting.'
+sleep 0.2
+clear
+echo 'starting..'
+sleep 0.2
+clear
+echo 'starting...'
+sleep 0.2
+clear
+echo 'starting....'
+sleep 0.2
+clear
+echo 'starting.'
+sleep 0.2
+clear
+echo 'starting..'
+sleep 0.2
+clear
+echo 'starting...'
+sleep 0.2
+clear
+echo 'starting....'
+sleep 0.2
+clear
+echo 'starting.'
+sleep 0.2
+clear
+echo 'starting..'
+sleep 0.2
+clear
+echo 'starting...'
+sleep 0.2
+clear
+echo 'starting....'
+sleep 0.2
+clear
+echo 'starting.'
+sleep 0.2
+clear
+echo 'starting..'
+sleep 0.2
+clear
+echo 'starting...'
+sleep 0.2
+clear
+echo 'starting....'
+clear
+echo 'write help if you need help'
+read -p "enter a command: " command
+if [ "$command" == "help" ]
+then
+    echo 'commands:'
+    echo 'exit'
+    echo 'update'
+    echo 'start'
+elif [ "$command" == "exit" ]
+then
+    exit
+elif [ "$command" == "update" ]
+then
+    bash update.sh
+    read -p "enter a command: " command
+elif [ "$command" == "start" ]
+then
+    python 3 notepad.py
+else
+    echo "invalid command"
+    read -p "enter a command: " command
+fi
+    
